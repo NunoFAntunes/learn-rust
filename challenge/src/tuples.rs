a374e4eb752372eac7807fcb329b40e822327968
Nuno Antunes <nfcga@iscte-iul.pt>
2021-04-12 16:03:00 +0100

    tuples

---
+++ challenge/src/tuples.rs
@@ -0,0 +1,9 @@
+// Tuples group vals of different types
+// Max 12 elements
+
+pub fn run() {
+    let person: (&str, &str, i8) = ("Nuno", "Lisboa", 24);
+
+    println!("{} is from {} and is {}", person.0, person.1, person.2);
+
+}
