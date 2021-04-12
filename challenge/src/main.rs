d2697343bcd2ad9fb41373e33e6d44ec6f6c8107
Nuno Antunes <nfcga@iscte-iul.pt>
2021-04-12 16:36:45 +0100

    Loops

--- challenge/src/main.rs
+++ challenge/src/main.rs
@@ -6,7 +6,8 @@
 // mod tuples;
 // mod arrays;
 // mod vectors;
-mod conditionals;
+// mod conditionals;
+mod loops;
 
 fn main() {
 //    print::run();
@@ -16,5 +17,6 @@ fn main() {
 //    tuples::run();
 //    arrays::run();
 //    vectors::run();
-conditionals::run();
+// conditionals::run();
+    loops::run();
 }
