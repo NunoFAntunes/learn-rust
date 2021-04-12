d3b89c61876cc85262a6e1c3ec38b7ea4e0d9344
Nuno Antunes <nfcga@iscte-iul.pt>
2021-04-12 15:44:12 +0100

    Data types

--- challenge/src/types.rs
+++ challenge/src/types.rs
@@ -8,6 +8,34 @@ Tuples
 Arrays
 */
 
-pub fn run() {
+// Statically Typed Language.
+// Compiler must know var type.
+// Usually compiler can infer the type based on the value and how we use it
 
+pub fn run() {
+    // Default is "i32"
+    let x = 1;
+
+    // Default is "f64"
+    let y = 2.5;
+
+    // Add explicit type
+    let z: i64 = 23478649462347896;
+
+    // FInd max size
+    println!("Max i32: {}", std::i32::MAX);
+    println!("Max i32: {}", std::i64::MAX);
+
+    // Boolean
+    let is_active: bool = true;
+
+    // Get boolean from expression
+    let is_greater_than = 10 < 5;
+
+    println!("{:?}", (x, y, z, is_active, is_greater_than));
+
+    // Char
+    let a1 = 'a';
+    let face = '\u{1F600}';
+    println!("{:?}", (a1, face));
 }
